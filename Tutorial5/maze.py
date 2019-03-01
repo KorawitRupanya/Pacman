533db6e35ea7bfd9c5bb816defce125390dade47
KorawitRupanya <smart1micky268@gmail.com>
2019-03-01 14:52:41 +0700

    first commit

---
+++ Tutorial5/maze.py
@@ -0,0 +1,82 @@
+import arcade
+from models import World, Pacman, Maze
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BLOCK_SIZE = 40
+ 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+ 
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+class MazeDrawer():
+    def __init__(self, maze):
+        self.maze = maze
+        self.width = self.maze.width
+        self.height = self.maze.height
+ 
+        self.wall_sprite = arcade.Sprite('images/wall.png')
+        self.dot_sprite = arcade.Sprite('images/dot.png')
+    
+    def get_sprite_position(self, r, c):
+        x = c * BLOCK_SIZE + (BLOCK_SIZE // 2);
+        y = r * BLOCK_SIZE + (BLOCK_SIZE + (BLOCK_SIZE // 2));
+        return x,y
+    
+    def draw_sprite(self, sprite, r, c):
+        x, y = self.get_sprite_position(r, c)
+        sprite.set_position(x, y)
+        sprite.draw()
+ 
+    def draw(self):
+        for r in range(self.height):
+            for c in range(self.width):
+                if self.maze.has_wall_at(r,c):
+                    self.draw_sprite(self.wall_sprite, r, c)
+                elif self.maze.has_dot_at(r,c):
+                    self.draw_sprite(self.dot_sprite, r, c )
+
+class MazeWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+ 
+        arcade.set_background_color(arcade.color.WHITE)
+ 
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
+        self.pacman_sprite = ModelSprite('images/pacman.png',
+                                         model=self.world.pacman)
+         
+        self.maze_drawer = MazeDrawer(self.world.maze)
+      
+ 
+ 
+    def update(self, delta):
+        self.world.update(delta)
+ 
+    def on_draw(self):
+        arcade.start_render()
+ 
+        self.maze_drawer.draw()                
+        self.pacman_sprite.draw()
+    
+    def on_key_press(self, key, key_modifiers):
+         self.world.on_key_press(key, key_modifiers)
+ 
+def main():
+    window = MazeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    arcade.run()
+ 
+if __name__ == '__main__':
+    main()
---
+++ Tutorial5/models.py
@@ -0,0 +1,126 @@
+import arcade.key
+
+DIR_STILL = 0
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+MOVEMENT_SPEED = 4
+
+KEY_MAP = { arcade.key.UP: DIR_UP,
+            arcade.key.DOWN: DIR_DOWN,
+            arcade.key.LEFT: DIR_LEFT,
+            arcade.key.RIGHT: DIR_RIGHT, }
+ 
+DIR_OFFSETS = { DIR_STILL: (0,0),
+                DIR_UP: (0,1),
+                DIR_RIGHT: (1,0),
+                DIR_DOWN: (0,-1),
+                DIR_LEFT: (-1,0) }
+
+class Pacman:
+    def __init__(self, world, x, y, maze, block_size):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.direction = DIR_STILL
+        self.maze = maze
+ 
+        self.block_size = block_size
+        self.next_direction = DIR_STILL
+    
+    def is_at_center(self):
+        half_size = self.block_size // 2
+        return (((self.x - half_size) % self.block_size == 0) and
+                ((self.y - half_size) % self.block_size == 0))
+ 
+    def move(self, direction):
+        self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
+        self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
+
+    def check_walls(self,direction):
+        new_r = DIR_OFFSETS[direction][1]+self.get_row()
+        new_c = DIR_OFFSETS[direction][0]+self.get_col()
+        return not self.maze.has_wall_at(new_r, new_c)
+ 
+    def check_dots(self):
+        if(self.has_dot==True)
+            return True
+        else:
+            return False
+ 
+    def update(self, delta):
+        if self.is_at_center():
+            if self.check_walls(self.next_direction):
+                self.direction = self.next_direction
+            else:
+                self.direction = DIR_STILL
+ 
+        self.move(self.direction)
+ 
+        if self.is_at_center():
+            self.check_dots()
+    
+    def get_row(self):
+        return (self.y - self.block_size) // self.block_size
+ 
+    def get_col(self):
+        return self.x // self.block_size
+
+class World:
+    def __init__(self, width, height, block_size):
+        self.width = width
+        self.height = height
+        self.block_size = block_size
+        self.maze = Maze(self)
+ 
+        self.pacman = Pacman(self, 60, 100,
+                             self.maze,
+                             self.block_size)
+        
+
+    def on_key_press(self, key, key_modifiers):
+        if key in KEY_MAP:
+            self.pacman.next_direction = KEY_MAP[key]
+
+    def update(self, delta):
+        self.pacman.update(delta)
+    
+class Maze:
+    def __init__(self, world):
+        self.map = [ '####################',
+                     '#..................#',
+                     '#.###.###..###.###.#',
+                     '#.#...#......#...#.#',
+                     '#.#.###.####.###.#.#',
+                     '#.#.#..........#.#.#',
+                     '#.....###..###.....#',
+                     '#.#.#..........#.#.#',
+                     '#.#.###.####.###.#.#',
+                     '#.#...#......#...#.#',
+                     '#.###.###..###.###.#',
+                     '#..................#',
+                     '####################' ]
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.init_dot_data()
+ 
+    def init_dot_data(self):
+        has_dot = {}
+        for r in range(self.height):
+            has_dot[r] = {}
+            for c in range(self.width):
+                has_dot[r][c] = self.map[r][c] == '.'
+        self.has_dot = has_dot
+ 
+    def has_wall_at(self, r, c):
+        return self.map[r][c] == '#'
+    
+    def has_dot_at(self, r, c):
+        return self.has_dot[r][c]
+    
+    def remove_dot_at(self, r, c):
+        self.has_dot[r][c] = False
+ 
+    def update(self, delta):
+        self.pacman.update(delta)
